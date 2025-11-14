28f318a7bd80290f2c264c685780109a38cc14c5
Eduardo Vieira <eduardo.ro.vieira@gmail.com>
2025-11-14 20:58:01 -0300

    feat: novos festures de drag and drop e correções visuais

--- home.js
+++ home.js
@@ -43,7 +43,10 @@ function inicializeAll(formId, inputId, taskId) {
         }
         const taskItem = document.createElement("li");
 
-        taskItem.innerHTML = `<span>${newTask}</span> <button class="remove-btn">🗑️</button><button class="completed-btn">✅</button>`;
+        taskItem.innerHTML = `<span>${newTask}</span> 
+        <button class=" completed-btn"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></button>
+        <button class="remove-btn"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg></button>`;
+
         taskItem.setAttribute("draggable", "true");
         taskItem.id = "task-" + Date.now();
         taskItem.addEventListener("dragstart", dragStart);
@@ -54,24 +57,28 @@ function inicializeAll(formId, inputId, taskId) {
     }
 
     function manageTask(event) {
-        if (event.target.classList.contains("remove-btn")) {
-        const taskItem = event.target.parentElement.remove();
+
+        const removeButton = event.target.closest(".remove-btn");
+
+        if (removeButton) {
+            removeButton.parentElement.remove();
 
         return;
         }
 
-        if (event.target.classList.contains("completed-btn")) {
-        const completedButton = event.target;
+
+        const completedButton = event.target.closest(".completed-btn");
+        if (completedButton) {
         const listItem = completedButton.parentElement; //li que tem o botão (filho).
         const list = listItem.parentElement; //lista que contem o li (filho).
         const textSpan = listItem.querySelector("span");
 
         textSpan.classList.toggle("completed");
         if (textSpan.classList.contains("completed")) {
-            completedButton.innerHTML = "♻️";
+            completedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`;
             list.appendChild(listItem);
         } else {
-            completedButton.innerHTML = "✅";
+            completedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`;
             list.insertBefore(listItem, list.firstChild);
         }
         }
